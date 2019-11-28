9ae27548e7be980e024c5e6506ea1efa62180966
Jawad Amir <jawad.amir@selise.com.bd>
2019-11-28 21:23:49 +0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+{id: 11, name:  'Mr,jawad' },
+{id: 12, name:  'Mr,jawad' },
+{id: 13, name:  'Mr,jawad' },
+{id: 14, name:  'Mr,jawad' },
+{id: 15, name:  'Mr,jawad' },
+{id: 16, name:  'Mr,jawad' },
+{id: 17, name:  'Mr,jawad' },
+{id: 18, name:  'Mr,jawad' },
+{id: 19, name:  'Mr,jawad' },
+{id: 20, name:  'Mr,jawad' }
+];
